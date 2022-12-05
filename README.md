98bd34a41da84b7f5b24e713ad8385440440d7a5
Brian Muller <bamuller@gmail.com>
2022-12-05 18:44:31 -0500

    Cache result by commit SHA, support must-match-ref

--- README.md
+++ README.md
@@ -1,8 +1,62 @@
-steps:
-  - uses: parallel-markets/sha-storage-action
+# SHA Storage Action
 
-later
-  - uses: parallel-markets/sha-storage-action
-    if: tests pass
-    with:
-      result: success
+Store a result for the given SHA of a repo.
+
+## Example
+
+Here's a simple example where a test job will only run once for a given version of the codebase (rerunning the job won't result in `make test` running more than once, if the last run succeeded).
+
+```yaml
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - id: last-run
+        uses: bmuller/sha-storage-action@master
+
+      - run: make test
+        if: steps.last-run.outputs.result != 'success'
+
+      - uses: bmuller/sha-storage-action@master
+        with:
+          result: success
+```
+
+Here's an example where you may only want the "test" job to run successfully at most once:
+
+```yaml
+jobs:
+  fetch-prev-result:
+    runs-on: ubuntu-latest
+    outputs:
+      prev-result: ${{ steps.last-run.outputs.result }}
+    steps:
+      - id: last-run
+        uses: bmuller/sha-storage-action@master
+
+  test:
+    runs-on: ubuntu-latest
+    needs: fetch-prev-result
+    if: needs.fetch-prev-result.outputs.prev_result != 'success'
+    steps:
+      - run: make test
+      - uses: bmuller/sha-storage-action@master
+        with:
+          result: success
+```
+
+And here's an example of how you can ensure the current checked out SHA matches a REF before checking/writing a result (to ensure you don't write a result for something that isn't the current HEAD of a branch or tag etc):
+
+```yaml
+jobs:
+  fetch-prev-result:
+    runs-on: ubuntu-latest
+    outputs:
+      prev-result: ${{ steps.last-run.outputs.result }}
+    steps:
+      - id: last-run
+        uses: bmuller/sha-storage-action@master
+        with:
+          must-match-ref: heads/main
+          token: ${{ secrets.GITHUB_TOKEN }}
+```
--- index.js
+++ index.js
@@ -5,21 +5,47 @@ const fs = require('fs')
 
 const RESULT_PATH = '/tmp/prev-result'
 
-try {
-  let result = core.getInput('result')
+const run = async () => {
   const sha = github.context.sha
-  core.info('Fetching status from core sha ' + sha)
+  core.info(`Running for current SHA ${sha}`)
+  
+  try {
+    const ref = core.getInput('must-match-ref')
 
-  const key = 'sha-storage-action-' + sha + '-' + Math.floor(Date.now() / 1000)
-  const cacheKey = await cache.restoreCache([RESULT_PATH], key, ['sha-storage-action-' + sha])
+    if (ref !== 'any') {
+      core.info(`Looking for ref ${ref}`)
+      const { owner, repo } = github.context.repo
+      const token = core.getInput('token')
+      if (token === '') {
+        core.setFailed('You must set the token input parameter with the value of ${{ secrets.GITHUB_TOKEN }}')
+        return
+      }
+      
+      const octokit = github.getOctokit(token)
+      // this will error out if the ref cannot be found
+      const refResult = await octokit.rest.git.getRef({ owner, repo, ref })
+      core.info(`Ref ${ref} has SHA of ${refResult.data.object.sha}`)
+      if (refResult.data.object.sha !== sha) {
+        core.setFailed(`Ref ${ref} does not match current code SHA`)
+        return
+      }
+    }
 
-  if (result !== 'unknown') {
-    fs.writeFileSync(RESULT_PATH, result)
-  } else if (fs.existsSync(RESULT_PATH)) {
-    result = fs.readFileSync(RESULT_PATH)
+    let result = core.getInput('result')
+    const key = 'sha-storage-action-' + sha + '-' + Math.floor(Date.now() / 1000)
+    const cacheKey = await cache.restoreCache([RESULT_PATH], key, ['sha-storage-action-' + sha])
+
+    if (result !== 'unknown') {
+      fs.writeFileSync(RESULT_PATH, result)
+      await cache.saveCache([RESULT_PATH], key)
+    } else if (fs.existsSync(RESULT_PATH)) {
+      result = fs.readFileSync(RESULT_PATH, { encoding: 'utf8' })
+    }
+
+    core.setOutput('result', result)
+  } catch(error) {
+    core.setFailed(error.message)
   }
-
-  core.setOutput('result', result)
-} catch(error) {
-  core.setFailed(error.message)
 }
+
+run()
